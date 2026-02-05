bea5bf8638bde35f8183c6147c13c0303d88999f
Stephan <stepandegtsiaryk@syndi-g.com>
2026-02-05 13:52:53 +0300

    Initial Commit

---
+++ Imperial piggy bank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Imperial piggy bank
+//
+//  Created by Stepan Degtsiaryk on 5.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Imperial piggy bank/Imperial_piggy_bankApp.swift
@@ -0,0 +1,17 @@
+//
+//  Imperial_piggy_bankApp.swift
+//  Imperial piggy bank
+//
+//  Created by Stepan Degtsiaryk on 5.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Imperial_piggy_bankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
